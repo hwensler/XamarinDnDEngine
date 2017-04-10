3cb73abb2bf016e01f4884df704658323c7c700b
hw <heatherwensler@gmail.com>
2017-04-10 10:33:52 -0700

    This is just Heather's personal project 2. We can add in Matt's character classes.

---
+++ App11/App11/ViewModels/AboutViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App11.ViewModels
+{
+	public class AboutViewModel : BaseViewModel
+	{
+        public AboutViewModel()
+        {
+            Title = "About";
+        }
+	}
+}
---
+++ App11/App11/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Xamarin.Forms;
+using System;
+
+namespace App11.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+            //the entire string display is below. it displays the current datetime as well as static strings
+            entireDisplay.Text = "CPSC - 5910 - SQ\nStudent - Heather Wensler\n" + DateTime.Now.ToString() + "\nCode Review - Jonathan Adler";
+        }
+	}
+}
