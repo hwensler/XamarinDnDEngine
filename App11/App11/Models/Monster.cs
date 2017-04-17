9bc0331f23d60dc59cf491740ae3acddcbc6ab9c
hw <wenslerh@seattleu.edu>
2017-04-17 16:08:06 -0700

    Put Monster in it's own C# file for clarity

---
+++ App11/App11/Models/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App11.Models
+{
+    public class Monster : Fighter
+    {
+        //monster constructor
+        public Monster(int strength, int defense, int speed, int stackOrder, int hitPoints)
+        {
+            this.strength = strength;
+            this.defense = defense;
+            this.speed = speed;
+            this.stackOrder = stackOrder;
+            this.hitPoints = hitPoints;
+        }
+    }
+}
