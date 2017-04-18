92f65748e14f9d6117f337890a4fcc9c3609a378
hw <wenslerh@seattleu.edu>
2017-04-17 20:56:48 -0700

    Fixed test GetMonsterStrength to equal 10 instead of 1. Added test for ChangeHP in Monster

--- Unit Test/MonsterTests.cs
+++ Unit Test/MonsterTests.cs
@@ -9,10 +9,19 @@ namespace Unit_Test
     {
         Monster testMonster = new Monster(10, 10, 10, 10, 10, 10);
 
+        //just testing a getter
         [TestMethod]
         public void GetMonsterStrengthWorks()
         {
             Assert.AreEqual(testMonster.Strength, 1);
+        }
+
+        //test doing an amount of damage to the defender
+        [TestMethod]
+        public void ChangeHPWorks()
+        {
+            testMonster.ChangeHP(1);
+            Assert.AreEqual(testMonster.HitPoints, 11);
         }
     }
 }
