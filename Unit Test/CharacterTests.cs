5bb4c737fb46d21af873d6bbb65b7f3796920376
hw <wenslerh@seattleu.edu>
2017-04-17 21:26:08 -0700

    Added testCharacter to CharacterTests - all attributes initialized to 10

--- Unit Test/CharacterTests.cs
+++ Unit Test/CharacterTests.cs
@@ -2,13 +2,17 @@
 using App11.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+/**
+ * This class will only test things unique to the character class.
+ * For tests on the functionality of things inherited from Fighter.cs, check MonsterTests.cs
+ * **/
+
 namespace Unit_Test
 {
     [TestClass]
     public class CharacterTests
     {
-        [TestInitialize]
-
+        Character testCharacter = new Character(10, 10, 10, 10, 10);
         
         [TestMethod]
         public void TestMethod1()
