660c24ec2a723192c0a1f648249cc39b60a3fd51
Sunny Yeung <freezingstormy@gmail.com>
2017-04-24 04:36:06 -0700

    Added some viewmodels for monster class as well as monster pages, haven't finished yet

---
+++ App11/App11/ViewModels/MonsterDetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App11.Models;
+
+
+namespace App11.ViewModels
+{
+    public class MonsterDetailViewModel : BaseViewModel
+    {
+        public Monster Mon { get; set; }
+        public MonsterDetailViewModel (Monster mon = null)
+        {
+            Title = mon.Name;
+            Mon = mon;
+        }
+
+        int quantity = 1;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { SetProperty(ref quantity, value); }
+        }
+    
+        public Monster getMonster()
+        {
+            return Mon;
+        }
+
+    }
+}
