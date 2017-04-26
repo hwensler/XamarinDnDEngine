951a9ec3797c4b26c3df7c581e2acd0b518a9997
Matthew Irwin <irwinm1@seattleu.edu>
2017-04-26 15:48:45 -0700

    Added UWP DB Connection, DB still does not work for UWB when trying to access character screen.

---
+++ App11/App11.UWP/DatabaseConnection_UWP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using Xamarin.Forms;
+using App11.UWP;
+using Windows.Storage;
+using System.IO;
+using App11.Services;
+[assembly: Dependency(typeof(DatabaseConnection_UWP))]
+
+namespace App11.UWP
+{
+    public class DatabaseConnection_UWP : IDatabaseConnection
+    {
+        public SQLiteConnection DbConnection()
+        {
+            var dbName = "D&DDB.db3";
+            var path = Path.Combine(ApplicationData.
+              Current.LocalFolder.Path, dbName);
+            return new SQLiteConnection(path);
+        }
+    }
+}
