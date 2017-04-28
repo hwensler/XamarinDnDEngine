a0105a05df2bc4d30335d66afd46da39263c6b64
Matthew Irwin <irwinm1@seattleu.edu>
2017-04-28 14:05:51 -0700

    UWP DB for Char now working. Added IFileHelper.

---
+++ App11/App11.UWP/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+using App11.UWP;
+using Windows.Storage;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace App11.UWP
+{
+    class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
