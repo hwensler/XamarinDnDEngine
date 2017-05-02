fdbe9f768d940f04c950fcd95a5224762904e40d
Sunny Yeung <freezingstormy@gmail.com>
2017-05-01 17:54:51 -0700

    Added some comments for better understandability

--- App11/App11/Views/MonstersPage.xaml.cs
+++ App11/App11/Views/MonstersPage.xaml.cs
@@ -14,19 +14,22 @@ namespace App11.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MonstersPage : ContentPage
 	{
+        //to access the database
         DBDataAccess DNDDatabase;
 		public MonstersPage ()
 		{
 			InitializeComponent ();
+            //initializing
             this.DNDDatabase = new DBDataAccess();
         }
         //display the Monsters
         protected override void OnAppearing()
         {
             base.OnAppearing();
+            //for binding to the database
             this.BindingContext = this.DNDDatabase;
         }
-
+        //this method deals with selecting an item on the list view
         async void OnSelected(object sender, SelectedItemChangedEventArgs args)
         {
             var itemSelected = args.SelectedItem as Monster;
@@ -36,6 +39,7 @@ namespace App11.Views
             await Navigation.PushAsync(new MonsterDetailPage(
                 new MonsterDetailViewModel(itemSelected)));
 
+            //the listview name on the xaml page
             MonstersList.SelectedItem = null;
         }
         //add a new monster
