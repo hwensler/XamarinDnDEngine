aa313b7cc60c7a3686728b7552b711bb3831e802
Sunny Yeung <freezingstormy@gmail.com>
2017-05-01 18:00:42 -0700

    Added some comments on new monster page and took out some unneeded functions.

--- App11/App11/Views/NewMonsterPage.xaml.cs
+++ App11/App11/Views/NewMonsterPage.xaml.cs
@@ -14,38 +14,37 @@ namespace App11.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewMonsterPage : ContentPage
     {
-
-        public Monster Monsters { get; set; }
-        DBDataAccess monAccess;
+        
+        public Monster Monsters { get; set; }           //to store monster to use on this page
+       
+        DBDataAccess monAccess;                         //start a database access
         public NewMonsterPage()
         {
             InitializeComponent();
-            monAccess = new DBDataAccess();
-            Title = "new entry";
-            Monsters = new Monster
+            monAccess = new DBDataAccess();             //intialize the database
+            Title = "New Entry";                        //bind from xaml page
+            Monsters = new Monster                      //intialize a new monster
             {
-                Name = "name",
-                Description = "description here"
+                Name = "Name of Monster",
+                Description = "I am a gruesome detail of a monster."
             };
-            BindingContext = this;
+            BindingContext = this;                      //bind it to our xaml page
         }
+        //this method takes in a monster class that allows editing of its name and description only
         public NewMonsterPage(Monster edit)
         {
             InitializeComponent();
             monAccess = new DBDataAccess();
-            Title = "change monster";
+            Title = "Change Monster details";
             Monsters = edit;
-            BindingContext = this;
+            BindingContext = this;                      //bind to display the current details
         }
+        //this method upon clicking of save will update the monster details.
         async void Save_Clicked(object sender, EventArgs e)
         {
             monAccess.SaveOrUpdateMonster(Monsters);
             await Navigation.PopToRootAsync();
         }
 
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
-        {
-
-        }
     }
 }
