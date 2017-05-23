0c6cae8bc5127b295abbd5b4bbeb17cbdbaf3f17
Matthew Irwin <irwinm1@seattleu.edu>
2017-05-22 23:35:38 -0700

    Switches added to settings tab

--- App11/App11/Views/SettingPage.xaml.cs
+++ App11/App11/Views/SettingPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using App11.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,8 @@ namespace App11.Views
 		public SettingPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+            BindingContext = Setting.critMiss;
+        }
+
+    }
 }
