60caa1fefbefb27561177202c8d87f9cfee413aa
Sunny Yeung <freezingstormy@gmail.com>
2017-05-23 04:41:19 -0700

    Added counter to item

--- App11/App11/Models/Item.cs
+++ App11/App11/Models/Item.cs
@@ -7,24 +7,24 @@ namespace App11.Models
     [Table("Items")]
     public class Item : BaseDataObject
     {
-        
+
         public String itemId { get; set; }
 
         //the name of an item
         string name = string.Empty;
         public string Name
-		{
-			get { return name; }
-			set { SetProperty(ref name, value); }
-		}
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
 
         //the description of an item
-		string description = string.Empty;
-		public string Description
-		{
-			get { return description; }
-			set { SetProperty(ref description, value); }
-		}
+        string description = string.Empty;
+        public string Description
+        {
+            get { return description; }
+            set { SetProperty(ref description, value); }
+        }
 
         //the strength of the item
         public int Strength { get; set; }
@@ -32,6 +32,19 @@ namespace App11.Models
         //tells what attribute (strength, defense, speed, hp) the item modifies
         public string Attribute { get; set; }
 
-
+        //item usage set to 100 uses
+        private int _itemCounter;
+        public int ItemCounter
+        {
+            get
+            {
+                return _itemCounter;
+            }
+            set
+            {
+                _itemCounter = 100;
+            }
+        }
     }
+        
 }
