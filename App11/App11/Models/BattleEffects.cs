9b899a34510ed2a630f788de597f577226f9a9fd
Sunny Yeung <freezingstormy@gmail.com>
2017-05-23 05:32:12 -0700

    Added battleeffects model

---
+++ App11/App11/Models/BattleEffects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App11.Models
+{
+    public class BattleEffects
+    {
+        //Name to show in output
+        public string Name { get; set; }
+
+        //More information about the effect
+        public string Description { get; set; }
+
+        //Amount of Impact Positive or Negative
+        public int Tier { get; set; }
+
+        //Who gets impacted HARACTERSINGLE, CHARACTERALL, MONSTERAALL, MONSTERSINGLE, ALL
+        public string Target { get; set; }
+
+        //The Attribute Modified {STRENGTH, SPEED, DEFENSE, HP }
+        public string AttribMod { get; set; }
+    }
+}
