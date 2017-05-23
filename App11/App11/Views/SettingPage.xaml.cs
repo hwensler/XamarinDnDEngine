789391004a824d47e1b458a139016a46fb2795ac
Matthew Irwin <irwinm1@seattleu.edu>
2017-05-23 00:27:43 -0700

    Change Toggles now update global setting class. This should be a big step in the implementation

--- App11/App11/Views/SettingPage.xaml.cs
+++ App11/App11/Views/SettingPage.xaml.cs
@@ -15,7 +15,48 @@ namespace App11.Views
 		public SettingPage ()
 		{
 			InitializeComponent ();
-            BindingContext = Setting.critMiss;
+        }
+
+        public void flipCrit(object sender, EventArgs e)
+        {
+            Setting.critHit = !Setting.critHit;
+        }
+        public void flipMiss(object sender, EventArgs e)
+        {
+            Setting.critMiss = !Setting.critHit;
+        }
+        public void flipIU(object sender, EventArgs e)
+        {
+            Setting.itemUsage = !Setting.critHit;
+        }
+        public void magicU(object sender, EventArgs e)
+        {
+            Setting.magicUsage = !Setting.critHit;
+        }
+        public void toggleHP(object sender, EventArgs e)
+        {
+            Setting.hpUsage = !Setting.critHit;
+        }
+        public void battleE(object sender, EventArgs e)
+        {
+            Setting.battleEvents = !Setting.critHit;
+        }
+        public void serverIt(object sender, EventArgs e)
+        {
+            Setting.useServerItems = !Setting.critHit;
+        }
+        public void randomIt(object sender, EventArgs e)
+        {
+            Setting.randomItems = !Setting.critHit;
+        }
+        public void superIt(object sender, EventArgs e)
+        {
+            Setting.superItems = !Setting.critHit;
+        }
+
+        public void debugIt(object sender, EventArgs e)
+        {
+            Setting.debugMode = !Setting.critHit;
         }
 
     }
