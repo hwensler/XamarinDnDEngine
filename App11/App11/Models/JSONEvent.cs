9ba2937788c2cd9124de1e245b0e33c44d3b9dd9
Matthew Irwin <irwinm1@seattleu.edu>
2017-05-23 22:08:30 -0700

    Battle Event Stuff from leon implemented

---
+++ App11/App11/Models/JSONEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App11.Models;
+
+namespace App11.Models
+{
+    class JSONEvent
+    {
+        int error_code;
+        string msg;
+        public List<ServerEvent> data;
+    }
+}
